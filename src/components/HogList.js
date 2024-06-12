65c89cfb49a35af3cd698e8e0c680217e2f7d02c
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-12 14:00:24 -0400

    Add sort functionality to app

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -1,25 +1,36 @@
 import React, { useState } from "react";
+import SortDropdown from "./SortDropdown";
 import HogTile from "./HogTile";
 
 const HogList = ({ hogs }) => {
 	const [showGreased, setShowGreased] = useState(false)
+	const [sortValue, setSortValue] = useState('')
 	
 	const handleFilterGreased = () => setShowGreased(showGreased => !showGreased)
+	const handleSort = (sortValue) => setSortValue(sortValue)
 
-	const filteredList = showGreased ? hogs.filter(hog => hog.greased) : hogs
+	const sortedList = [...hogs].sort((a, b) => {
+		if(sortValue === 'name'){
+			return a.name.localeCompare(b.name)
+		} else if(sortValue === 'weight') {
+			return a.weight - b.weight
+		}
+	})
+	const filteredList = showGreased ? sortedList.filter(hog => hog.greased) : sortedList
 	
 	return (
 		<>
 			<div>
-			<button onClick={handleFilterGreased}>Filter Greased Hogs</button>
+				<button onClick={handleFilterGreased}>Filter Greased Hogs</button>
 			</div>
+			<SortDropdown handleSort={handleSort} />
 			<div className="ui grid container">
 				<div className="ui eight wide column">
 					{filteredList.map(hog => {
 						return <HogTile key={hog.name} hog={hog} />
 					})}
 				</div>
-		</div>
+			</div>
 		</>
 	)
 }
